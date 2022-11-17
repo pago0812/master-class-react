130cd8c100bd1401f9015614a5e14c4534b045fa
Ana Lau <analaumg@gmail.com>
2022-11-16 21:15:08 -0600

    Version 3

--- src/header/Header.js
+++ src/header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 export const Header = () => {
   return (
     <div className="header-container">
-      <h1 className="h1-style">Pono app V2</h1>
+      <h1 className="h1-style">Pono app V3</h1>
     </div>
   );
 };
