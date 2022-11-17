9c5d50418a268ec40cb6abc3eb979bf3d5ad4b87
Ana Lau <analaumg@gmail.com>
2022-11-16 21:25:34 -0600

    V4 cambio en header grafiti

--- src/header/Header.js
+++ src/header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 export const Header = () => {
   return (
     <div className="header-container">
-      <h1 className="h1-style">Pono app V3</h1>
+      <h1 className="h1-style">Pono App V4 Ponysuavewashere</h1>
     </div>
   );
 };
