fb021692dc4a575e2819bb6d104b1d851a53bc73
Ana Lau <analaumg@gmail.com>
2022-11-16 21:35:45 -0600

    V5 removición de graffiti

--- src/header/Header.js
+++ src/header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 export const Header = () => {
   return (
     <div className="header-container">
-      <h1 className="h1-style">Pono App V4 Ponysuavewashere</h1>
+      <h1 className="h1-style">Pono App V4 </h1>
     </div>
   );
 };
